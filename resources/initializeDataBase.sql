6f4fe92e5a2cd6394c9bfa6243cb64082c640584
shairam <shaikrish27@gmail.com>
2018-09-28 14:00:07 +0530

    Update sql file

--- resources/initializeDataBase.sql
+++ resources/initializeDataBase.sql
@@ -4,13 +4,12 @@
  */
  
 -- Create EMPLOYEE_RECORDS database
-CREATE DATABASE IF NOT EXISTS EMPLOYEE_RECORDS;     
-
+CREATE DATABASE IF NOT EXISTS EMPLOYEE_RECORDS;
 -- Switch to EMPLOYEE_RECORDS database
 USE EMPLOYEE_RECORDS;
 
 -- create EMPLOYEES table in the database
 CREATE TABLE IF NOT EXISTS EMPLOYEES (EmployeeID INT, Name VARCHAR(50), Age INT, SSN INT, PRIMARY KEY (EmployeeID))
 
-INSERT INTO `student` (`Name`, `Age`, `SSN`, `mobNo`, `address`) VALUES
-('Alice',30,123456789);
+INSERT INTO `student` (`EmployeeID`,`Name`, `Age`, `SSN`, `mobNo`, `address`) VALUES
+(879796979,'Alice',30,123456789 );
